7e95379dea05deea3a00074e1c6f66c4b0dda6e4
brkozik <briankozik@gmail.com>
2017-02-25 14:48:09 -0700

    more instance vars

--- app/src/main/java/com/example/bkozik/butterfliesandhurricanes/MainActivity.java
+++ app/src/main/java/com/example/bkozik/butterfliesandhurricanes/MainActivity.java
@@ -1,12 +1,50 @@
 package com.example.bkozik.butterfliesandhurricanes;
 
+
+import java.io.File;
+import java.lang.ref.WeakReference;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import com.choosemuse.libmuse.MuseManagerAndroid;
+import com.choosemuse.libmuse.*;
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Looper;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.bluetooth.BluetoothAdapter;
+
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     private MuseManagerAndroid manager;
+    private Muse muse;
+    private ConnectionListener connectionListener;
+    private DataListener dataListener;
+
+    private final double[] eegBuffer = new double[6];
+    private boolean eegStale;
+    private final double[] alphaBuffer = new double[6];
+    private boolean alphaStale;
+    private final double[] accelBuffer = new double[3];
+    private boolean accelStale;
+
+    private final Handler handler = new Handler();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +53,53 @@ public class MainActivity extends AppCompatActivity {
 
         manager = MuseManagerAndroid.getInstance();
         manager.setContext(this);
+    }
+
+
+
+
+
+    class MuseL extends MuseListener {
+        final WeakReference<MainActivity> activityRef;
+
+        MuseL(final WeakReference<MainActivity> activityRef) {
+            this.activityRef = activityRef;
+        }
+
+        @Override
+        public void museListChanged() {
+            activityRef.get().museListChanged();
+        }
+    }
+
+    class ConnectionListener extends MuseConnectionListener {
+        final WeakReference<MainActivity> activityRef;
+
+        ConnectionListener(final WeakReference<MainActivity> activityRef) {
+            this.activityRef = activityRef;
+        }
+
+        @Override
+        public void receiveMuseConnectionPacket(final MuseConnectionPacket p, final Muse muse) {
+            activityRef.get().receiveMuseConnectionPacket(p, muse);
+        }
+    }
+
+    class DataListener extends MuseDataListener {
+        final WeakReference<MainActivity> activityRef;
+
+        DataListener(final WeakReference<MainActivity> activityRef) {
+            this.activityRef = activityRef;
+        }
+
+        @Override
+        public void receiveMuseDataPacket(final MuseDataPacket p, final Muse muse) {
+            activityRef.get().receiveMuseDataPacket(p, muse);
+        }
+
+        @Override
+        public void receiveMuseArtifactPacket(final MuseArtifactPacket p, final Muse muse) {
+            activityRef.get().receiveMuseArtifactPacket(p, muse);
+        }
     }
 }
