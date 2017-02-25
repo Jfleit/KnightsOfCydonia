7a8c5e3f2bc7574478841476b1b56bb526e53f7c
brkozik <briankozik@gmail.com>
2017-02-25 14:20:23 -0700

    Initial commit

---
+++ app/src/main/java/com/example/bkozik/butterfliesandhurricanes/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.bkozik.butterfliesandhurricanes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.choosemuse.libmuse.MuseManagerAndroid;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MuseManagerAndroid manager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
